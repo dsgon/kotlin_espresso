d1fb6478cd6608cffc48e31c7e9d6e96c392815f
francisco.meza <francisco.meza@MIDL002116>
2020-12-10 11:07:56 -0700

    Update Branch 12/10

--- app/src/androidTest/java/com/kotlin/espresso/pages/PageSignUp.kt
+++ app/src/androidTest/java/com/kotlin/espresso/pages/PageSignUp.kt
@@ -7,9 +7,38 @@ import org.hamcrest.Matcher
 
 class PageSignUp {
 
-    var inputUser = R.id.input_user
+
+    private var userSignUp = R.id.input_user
+    private var inpuEmailSignUp = R.id.input_email
+    private var inputPasswordSignUp = R.id.input_password
+    private var confirmPasswordSignUp = R.id.input_password2
+    private var signUpBtn = R.id.btn_sign_up
+    private var loginBtn = R.id.link_login
+
 
     fun getInputUser(): Matcher<View>? {
-        return  ViewMatchers.withId(inputUser)
+        return ViewMatchers.withId(userSignUp)
     }
+
+    fun getInputEmail(): Matcher<View>? {
+        return ViewMatchers.withId(inpuEmailSignUp)
+    }
+
+    fun getInputPassword(): Matcher<View>? {
+        return ViewMatchers.withId(inputPasswordSignUp)
+    }
+
+    fun getInputPassConfirmation(): Matcher<View>? {
+        return ViewMatchers.withId(confirmPasswordSignUp)
+    }
+
+    fun clickRegister(): Matcher<View>? {
+        return ViewMatchers.withId(signUpBtn)
+    }
+
+    fun clickLoginBtn(): Matcher<View>? {
+        return ViewMatchers.withId(loginBtn)
+    }
+
+
 }
--- app/src/androidTest/java/com/kotlin/espresso/steps/StepsSignUp.kt
+++ app/src/androidTest/java/com/kotlin/espresso/steps/StepsSignUp.kt
@@ -1,21 +1,32 @@
 package com.kotlin.espresso.steps
 
-import android.app.Activity
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
 import androidx.test.rule.ActivityTestRule
 import com.kotlin.espresso.pages.ErrorMessages
 import com.kotlin.espresso.pages.PageSignUp
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
 
 class StepsSignUp : BaseSteps() {
 
-    var page = PageSignUp()
+    var pageSignUp = PageSignUp()
 
-    fun setUser(user : String){
-        Espresso.onView(
-            page.getInputUser()
-        ).perform(ViewActions.typeText(user))
+    fun setUser(name: String, email: String, password: String, conPassowrd: String) {
+        onView(pageSignUp.getInputEmail())
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            .perform(ViewActions.typeText(name))
+        onView(pageSignUp.getInputPassword())
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            .perform(ViewActions.typeText(email))
+        onView(pageSignUp.getInputPassConfirmation())
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            .perform(ViewActions.typeText(conPassowrd))
+        closeKeyboard()
+        onView(pageSignUp.clickRegister())
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed())).perform(ViewActions.click())
     }
 
     fun validateLoginErrorMessage(activityRule : ActivityTestRule<AppCompatActivity>){
